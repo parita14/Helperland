46390a900b362b2ce4217fbfbcff7f33fcc8fdbe
Parita Solanki <85400399+parita14@users.noreply.github.com>
2022-01-29 15:09:35 +0530

    Create contactus.js

---
+++ Contact_Us_API/public/models/contactus.js
@@ -0,0 +1,96 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ContactUsModelAttributes = exports.ContactUs = void 0;
+var sequelize_1 = require("sequelize");
+var ContactUs = /** @class */ (function (_super) {
+    __extends(ContactUs, _super);
+    function ContactUs() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return ContactUs;
+}(sequelize_1.Model));
+exports.ContactUs = ContactUs;
+;
+exports.ContactUsModelAttributes = {
+    ContactUsId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: sequelize_1.DataTypes.INTEGER
+    },
+    Name: {
+        allowNull: false,
+        type: sequelize_1.DataTypes.STRING
+    },
+    Email: {
+        allowNull: false,
+        type: sequelize_1.DataTypes.STRING
+    },
+    SubjectType: {
+        allowNull: false,
+        type: sequelize_1.DataTypes.STRING
+    },
+    Subject: {
+        allowNull: true,
+        type: sequelize_1.DataTypes.STRING
+    },
+    PhoneNumber: {
+        allowNull: false,
+        type: sequelize_1.DataTypes.STRING
+    },
+    Message: {
+        allowNull: false,
+        type: sequelize_1.DataTypes.STRING
+    },
+    UploadFileName: {
+        allowNull: true,
+        type: sequelize_1.DataTypes.STRING
+    },
+    path: {
+        allowNull: true,
+        type: sequelize_1.DataTypes.STRING
+    },
+    CreatedBy: {
+        allowNull: true,
+        type: sequelize_1.DataTypes.INTEGER
+    },
+    Status: {
+        allowNull: true,
+        type: sequelize_1.DataTypes.INTEGER
+    },
+    Priority: {
+        allowNull: true,
+        type: sequelize_1.DataTypes.INTEGER
+    },
+    AssignedToUser: {
+        allowNull: true,
+        type: sequelize_1.DataTypes.INTEGER
+    },
+    IsDeleted: {
+        allowNull: false,
+        type: sequelize_1.DataTypes.BOOLEAN
+    },
+    createdAt: {
+        allowNull: false,
+        type: sequelize_1.DataTypes.DATE
+    },
+    updatedAt: {
+        allowNull: false,
+        type: sequelize_1.DataTypes.DATE
+    }
+};
