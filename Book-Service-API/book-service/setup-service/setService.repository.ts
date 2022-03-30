cea1d0e285132595b3f9f65837450c21e3396c15
Parita Solanki <85400399+parita14@users.noreply.github.com>
2022-03-30 17:45:38 +0530

    Delete Book-Service-API directory

--- Book-Service-API/book-service/setup-service/setService.repository.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "../../models/index";
-import { User } from "../../models/user";
-
-export class ServiceRepository {
-    
-    public async getSP(): Promise<User[]> {
-        return db.Users.findAll({ where: {UserTypeId: 2} });
-    }
-}
